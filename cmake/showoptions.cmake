4d66807eb7a1c0779ad8136af4e3241c458ac24d
Hendrik Flowers <flowershendrik@ymail.com>
2012-04-03 07:27:32 -0800

    Random Cleanup 4

--- cmake/showoptions.cmake
+++ cmake/showoptions.cmake
@@ -1,10 +1,10 @@
 # output generic information about the core and buildtype chosen
 message("")
-message("* FaceCore(pa) rev. hash  : ${rev_hash}")
-message("* FaceCore(pa) rev. date  : ${rev_date}")
+message("* ContiNuityCore rev. hash  : ${rev_hash}")
+message("* ContiNuityCore rev. date  : ${rev_date}")
 
 if( UNIX )
-  message("* FaceCore(pa) buildtype  : ${CMAKE_BUILD_TYPE}")
+  message("* ContiNuityCore buildtype  : ${CMAKE_BUILD_TYPE}")
 endif()
 message("")
 
